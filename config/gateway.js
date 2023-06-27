746f01e178760ee7c9b759f40ac9e23b9da0dcd5
ShaqEd92 <shaqhaedwards@gmail.com>
2023-06-27 12:04:38 -0500

    setup checkpoint

--- config/gateway.js
+++ config/gateway.js
@@ -4,12 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // <--- SET UP GATEWAY CONFIGURATION BELOW --->
-const gateway = new braintree.BraintreeGateway({
-  environment: braintree.Environment.Sandbox,
-  merchantId: '67mmk5hdbw6rt32',
-  publicKey: 'knt7xgn3hxb6fygj',
-  privateKey: 'a9fbf664baf2ecaf07fc759f69d7f666'
-});
+const gateway = "Replace this line with your API credentials"
 // <--- / END --->
 
 export default gateway;
--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 import gateway from "../config/gateway.js";
-import braintree from "braintree";
 
 export const index = async (req, res) => {
     try {
         const response = await gateway.clientToken.generate();
         var feedback = {status: "SUCCESS", data: gateway.config}
       } catch (exception) {
+        console.log(exception)
         var feedback = {status: "FAILURE", data: exception}
       }
     res.render("index", {feedback});
